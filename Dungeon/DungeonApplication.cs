f1aca482b8d5b8c93e2f39526a16a541438b5571
WilliamWatts <awnewatts97@outlook.com>
2022-11-08 20:21:03 -0600

    dungeon wireframe

---
+++ Dungeon/DungeonApplication.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon
+{
+    internal class DungeonApplication
+    {
+        static void Main(string[] args)
+        {
+            //TODO Create a player
+
+            bool counterMainLoop = true;
+
+            do
+            {
+                //TODO Create a Monster
+                //TODO Create a Room
+
+                bool counterInnerLoop = true;
+
+                do
+                {
+                    switch (switch_on)
+                    {
+                        case "ATTACK":
+                            break;
+                        case "RUN AWAY":
+                            break;
+                        case "CHARACTER INFO":
+                            break;
+                        case "MONSTER INFO":
+                            break;
+                        case "EXIT":
+                            break;
+                        default:
+                            break;
+                    }
+
+                } while (counterInnerLoop);
+
+            } while (counterMainLoop);
+        }//end main
+
+        private static string GetRoom()
+        {
+
+        }
+    }//end class
+}//end namespace
