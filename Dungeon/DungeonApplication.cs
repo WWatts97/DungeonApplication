6442a60e0e9e0e00e95a0415049bc7d2d5ccce88
WilliamWatts <awnewatts97@outlook.com>
2022-11-09 21:46:33 -0600

    get room

--- Dungeon/DungeonApplication.cs
+++ Dungeon/DungeonApplication.cs
@@ -12,41 +12,51 @@ namespace Dungeon
         {
             //TODO Create a player
 
-            bool counterMainLoop = true;
+            //bool counterMainLoop = true;
 
-            do
-            {
-                //TODO Create a Monster
-                //TODO Create a Room
+            //do
+            //{
+            //    //TODO Create a Monster
+            Console.WriteLine(GetRoom());
 
-                bool counterInnerLoop = true;
+            //    bool counterInnerLoop = true;
 
-                do
-                {
-                    switch (switch_on)
-                    {
-                        case "ATTACK":
-                            break;
-                        case "RUN AWAY":
-                            break;
-                        case "CHARACTER INFO":
-                            break;
-                        case "MONSTER INFO":
-                            break;
-                        case "EXIT":
-                            break;
-                        default:
-                            break;
-                    }
+            //    //do
+            //    //{
+            //    //    switch (switch_on)
+            //    //    {
+            //    //        case "ATTACK":
+            //    //            break;
+            //    //        case "RUN AWAY":
+            //    //            break;
+            //    //        case "CHARACTER INFO":
+            //    //            break;
+            //    //        case "MONSTER INFO":
+            //    //            break;
+            //    //        case "EXIT":
+            //    //            break;
+            //    //        default:
+            //    //            break;
+            //    //    }
 
-                } while (counterInnerLoop);
+            //    } while (counterInnerLoop);
 
-            } while (counterMainLoop);
+            //} while (counterMainLoop);
         }//end main
 
         private static string GetRoom()
         {
-
-        }
+            string[] roomDescriptions =
+            {
+                "This room is made of wood. It you can smell the age in the air.",
+                "You are filled with an overwhleming sensation of dread as you enter. The room reaks of death.",
+                "You are blinded. The room is so bright it cannot contain it all.",
+                "This room is smelly. That's it.",
+            };
+            Random rng = new Random();
+            int roomDescRandom = rng.Next(roomDescriptions.Length);
+            return $"{roomDescriptions[roomDescRandom]}";
+            
+        }//end GetRoom()
     }//end class
 }//end namespace
