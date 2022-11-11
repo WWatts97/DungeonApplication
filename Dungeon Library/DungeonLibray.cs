9031a9fd5156ed511e4fc7a6a10b3cff8286df24
WilliamWatts <awnewatts97@outlook.com>
2022-11-10 21:49:46 -0600

    added library and races and wepon types

---
+++ Dungeon Library/DungeonLibray.cs
@@ -0,0 +1,113 @@
+﻿namespace Dungeon_Library
+{
+    public class Character
+    {
+        //fields
+        private int _life;
+        //props
+        public string Name { get; set; }
+        public int HitChance { get; set; }
+        public int Block { get; set; }
+        public int MaxLife { get; set; }
+        public int Life
+        {
+            get { return _life; }
+            set
+            {
+                if (value <= MaxLife)
+                {
+                    _life = value;
+                }
+                else
+                {
+                    _life = MaxLife;
+                }
+            }
+        }
+        //ctors
+        public Character(string name, int hitChance, int block, int maxLife, int life)
+        {
+            Name = name;
+            HitChance = hitChance;
+            Block = block;
+            MaxLife = maxLife;
+            Life = life;
+        }
+        //    //methods
+        //    //TODO public decimal CalcBlock()
+        //    {
+
+        //    }
+        //    //TODO public decimal CalcHitChance()
+        //    {
+
+        //    }
+        //    //TODO public decimal CalcDamage() 
+        //    { 
+
+        //    }
+        public override string ToString()
+        {
+            return string.Format("Character Name: {0}\n" +
+                "Max Life: {1}\n" +
+                "Current Life: {2}\n" +
+                "Hit Chance: {3}\n" +
+                "Block? {4}",
+                Name,
+                MaxLife,
+                Life,
+                HitChance,
+                Block);
+        }
+    }//end character class
+
+    public class Weapon
+    {
+        //fields
+        private int _minDamage;
+        //props
+        public int MaxDamage { get; set; }
+        public string Name { get; set; }
+        public int BonusHitChance { get; set; }
+        public bool IsTwoHanded { get; set; }
+        public enum WeaponType { }
+        public int MinDamage
+        {
+            get { return _minDamage; }
+            set
+            {
+                if (value <= MaxDamage)
+                {
+                    _minDamage = value;
+                }
+                else
+                {
+                    _minDamage = MaxDamage;
+                }
+            }
+        }
+        //ctors
+        public Weapon(string name, int maxDamage, int minDamage, int bonusHitChance, bool isTwoHanded)
+        {
+            Name = name;
+            MaxDamage = maxDamage;
+            MinDamage = minDamage;
+            BonusHitChance = bonusHitChance;
+            IsTwoHanded = isTwoHanded;
+        }
+        //methods
+        public override string ToString()
+        {
+            return string.Format("Weapon Name: {0}\n" +
+                "Max Damage: {1}\n" +
+                "Min Damage: {2}\n" +
+                "Bonus Hit Chance: {3}\n" +
+                "Does it require both hands? {4}",
+                Name,
+                MaxDamage,
+                MinDamage,
+                BonusHitChance,
+                IsTwoHanded);
+        }
+    }//end wepaon class
+}
---
+++ Dungeon Library/Race.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Numerics;
+using System.Reflection.Emit;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon_Library
+{
+    public enum Race
+    {
+        Elf,
+        Human,
+        Merfolk,
+        Troll,
+        Gnome,
+        Demon
+    }
+        //8. Update the Weapon class to use the WeaponType enum
+
+        //a. Add a field/property for Type with is of data type WeaponType
+
+        //b.Add Type to the FQCTOR
+
+        //c.Add Type to the ToString()
+    public enum WeaponType
+    {
+        Sword,
+        Knife,
+        Axe,
+        Bow,
+        Lightsaber
+    }
+}
